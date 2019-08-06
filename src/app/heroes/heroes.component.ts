77bb437a280e39d91caae312ed4a2f8438b233a3
Szilvia Kapuvári <kapuvari.szilvia@gmail.com>
2019-08-06 23:04:14 +0200

    list heroes ngFor/Display heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,8 @@ hero: Hero = {
   id: 1,
   name: 'Windstorm'
 };
+
+heroes = HEROES;
 
   constructor() { }
 
